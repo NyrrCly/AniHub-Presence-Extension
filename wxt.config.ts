9a93cb5ebd3627ccf8391308a2731160092155bd
NyrrCly <baranchukmykola@gmail.com>
2026-08-20 00:18:43 +0300

    Update plugin version

--- wxt.config.ts
+++ wxt.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   modules: ['@wxt-dev/auto-icons'],
   manifest: {
     name: "AniHub Presence Extension",
-    version: "1.0.0",
+    version: "1.0.2",
     permissions: ["storage", "activeTab"],
     host_permissions: [
       "*://anihub.in.ua/*",
